90265e3c6b252e648469df3703be494637b0c318
JohnnyQianP <qianpengabc@126.com>
2021-03-26 15:50:48 +0800

    to git

---
+++ demotest/src/main/java/com/spboot/demotest/DemotestApplication.java
@@ -0,0 +1,14 @@
+package com.spboot.demotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotestApplication {
+
+    public static void main(String[] args) {
+        System.out.println("ok");
+        SpringApplication.run(DemotestApplication.class, args);
+    }
+
+}
